4f4a289c021accb79cb0a64a6023ebdc2ab33219
muhzin sidiq <muhzinsidhiq333@gmail.com>
2024-01-06 14:37:53 +0530

    create login and signup and signpup witj otp in server side

--- Server/src/server.js
+++ Server/src/server.js
@@ -5,12 +5,19 @@ import "dotenv/config";
 //  mongodb connection
 import connectToDb from '../Connection/config.js';
 
+import userRouter from "../routes/userRouter.js";
+
 const app=express()
 const PORT = process.env.PORT
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('tiny'));
+
+
+/*user route*/
+app.use("/api/auth", userRouter);
+/*user route*/
 
 connectToDb()
 
