832ef8f6aad9b0b374e79af516c20d99c321a8b6
muhzin sidiq <muhzinsidhiq333@gmail.com>
2024-01-08 13:28:16 +0530

    env file set up in frontend

--- Client/src/AxiosInterceptor/userAxiosInterceptor.js
+++ Client/src/AxiosInterceptor/userAxiosInterceptor.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with a base URL
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_baseURL,
   
 });
 
