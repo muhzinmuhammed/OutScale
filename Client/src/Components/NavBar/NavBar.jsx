9e51325a4fbeff4a46eb9fb626aac06a5b0113b8
muhzin sidiq <muhzinsidhiq333@gmail.com>
2024-01-09 12:42:53 +0530

    some changes in server side

--- Client/src/Components/NavBar/NavBar.jsx
+++ Client/src/Components/NavBar/NavBar.jsx
@@ -60,7 +60,7 @@ export default function NavBar() {
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
-                                    <h3 className='font-serif font-bold  text-white'></h3>
+                                    <h3 className='font-serif font-bold  text-white sm:hidden'>OutScale</h3>
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
